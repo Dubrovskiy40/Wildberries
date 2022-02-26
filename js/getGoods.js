7618f2bd5bf94387689ad9847a1924e2f22569fd
Dubrovskiy Anatoliy <78897480+Dubrovskiy40@users.noreply.github.com>
2022-02-26 23:58:02 +0300

    4
    4

--- js/getGoods.js
+++ js/getGoods.js
@@ -1,31 +1,78 @@
 const getGoods = () => {
     const links = document.querySelectorAll('.navigation-link');
+    const more = document.querySelector('.more');
 
-    const getData = () => {
+    const renderGoods = (goods) => {
+        const goodsContainer = document.querySelector('.long-goods-list');
+
+        goodsContainer.innerHTML = '';
+
+        goods.forEach((good) => {
+            const goodsBlock = document.createElement('div');
+
+            goodsBlock.classList.add('col-lg-3');
+            goodsBlock.classList.add('col-sm-6');
+            goodsBlock.data = good;
+
+            goodsBlock.innerHTML = `
+                <div class="goods-card">
+                    <span class="label ${good.label ? null : 'd-none'}">${good.label}</span>
+                    <img src="db/${good.img}" alt="${good.name}" class="goods-image">
+                    <h3 class="goods-title">${good.name}</h3>
+                    <p class="goods-description">${good.description}</p>
+                    <button class="button goods-card-btn add-to-cart" data-id="${good.id}">
+                        <span class="button-price">$${good.price}</span>
+                    </button>
+                </div>
+            `;
+
+            goodsContainer.append(goodsBlock);
+        })
+    };
+
+    const getData = (value, category) => {
         fetch('https://wildberriestest-dc6fa-default-rtdb.firebaseio.com/db.json')
             .then(response => response.json())
             .then(data => {
-                window.localStorage.setItem('wildberries', JSON.stringify({dataSet: 'data'}));
-                console.log(data);
+                const array = category ? data.filter(item => item[category] === value) : data;
+
+                localStorage.setItem('goods', JSON.stringify(array));
+
+                if (window.location.pathname !== '/goods.html') {
+                    window.location.href = '/goods.html';
+                } else {
+                    renderGoods(array);
+                }
+            })
+            .catch(err => {
+                console.log(err);
             })
     }
 
     links.forEach((link) => {
         link.addEventListener('click', (event) => {
             event.preventDefault();
+
+            const linkValue = link.textContent;
+            const category = link.dataset.field;
+
+            getData(linkValue, category);
+        })
+    });
+
+    // проверяем, если в localStorage что-то есть
+    if (localStorage.getItem('goods') && window.location.pathname === '/goods.html') {
+        renderGoods(JSON.parse(localStorage.getItem('goods')));
+    }
+
+    //клик по кнопке на главной странице
+    if (more) {
+        more.addEventListener('click', (event) => {
+            event.preventDefault();
+
             getData();
         })
-    })
-
-    const goods = JSON.parse(localStorage.getItem('wildberries'));
-    console.log(goods);
-
-    // localStorage.setItem('goods', JSON.stringify({name: 'all'}));
-    // const goods = JSON.parse(localStorage.getItem('goods'));
-    // console.log(goods);
-    // console.log(localStorage);
-    // localStorage.removeItem('goods');
-    // console.log(localStorage);
+    }
 };
 
 getGoods();
--- js/search.js
+++ js/search.js
@@ -1,16 +1,58 @@
 const search = () => {
-    const inputBlock = document.querySelector('.search-block');
+    // const inputBlock = document.querySelector('.search-block');
     const input = document.querySelector('.search-block > .form-control');
     const searchBtn = document.querySelector('.search-block > .btn-outline-secondary');
 
-    // inputBlock.addEventListener('click', (event) => {
-    //     event.preventDefault();
-    //     console.log(input.value);
-    // });
+    const renderGoods = (goods) => {
+        const goodsContainer = document.querySelector('.long-goods-list');
 
-    searchBtn.addEventListener('click', () => {
-        console.log(input.value);
-    })
+        goodsContainer.innerHTML = '';
+
+        goods.forEach((good) => {
+            const goodBlock = document.createElement('div');
+
+            goodBlock.classList.add('col-lg-3');
+            goodBlock.classList.add('col-sm-6');
+
+            goodBlock.innerHTML = `
+                <div class="goods-card">
+                    <span class="label ${good.label ? null : 'd-none'}">${good.label}</span>
+                    <img src="db/${good.img}" alt="${good.name}" class="goods-image">
+                    <h3 class="goods-title">${good.name}</h3>
+                    <p class="goods-description">${good.description}</p>
+                    <button class="button goods-card-btn add-to-cart" data-id="${good.id}">
+                        <span class="button-price">$${good.price}</span>
+                    </button>
+                </div>
+            `;
+
+            goodsContainer.append(goodBlock);
+        })
+    };
+
+    const getData = (value => {
+        fetch('https://wildberriestest-dc6fa-default-rtdb.firebaseio.com/db.json')
+            .then(response => response.json())
+            .then(data => {
+                const array = data.filter(good => good.name.toLowerCase().includes(value.toLowerCase()));
+
+                localStorage.setItem('goods', JSON.stringify(array));
+
+                if (window.location.pathname !== '/goods.html') {
+                    window.location.href = '/goods.html';
+                } else {
+                    renderGoods(array);
+                }
+            })
+    });
+
+    try {
+        searchBtn.addEventListener('click', () => {
+            getData(input.value)
+        })
+    } catch (error) {
+        console.error(error);
+    }
 };
 
 search();
